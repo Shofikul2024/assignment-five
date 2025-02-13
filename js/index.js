748b3f1b191601398a304b0fe97ea82c08978cd7
Shofikul2024 <islamshofi942@gmail.com>
2025-02-13 23:10:06 +0600

    header section added

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// donation section
+
+function getInputFieldValueById(id) {
+  const inputValue = parseFloat(document.getElementById(id).value);
+  return inputValue;
+}
+
+function getTextFieldValueById(id) {
+  const textField = parseFloat(document.getElementById(id).innerText);
+  return textField;
+}
+
+
+ 
